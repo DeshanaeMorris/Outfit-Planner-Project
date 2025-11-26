775f9520315686417e693b992940ba3d95571937
DeshanaeMorris <morrisdeshanae@gmail.com>
2025-11-26 00:23:12 -0500

    Edits

--- src/main/java/com/stylz/app/Main.java
+++ src/main/java/com/stylz/app/Main.java
@@ -1,4 +1,5 @@
 package com.stylz.app;
+import com.stylz.app.Firebase.FirestoreDatabase;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -8,11 +9,16 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/stylz/app/Login.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/stylz/app/StylzWelcome.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Outfit Planner");
         stage.setScene(scene);
         stage.show();
+    }
+
+    @Override
+    public void init() {
+        FirestoreDatabase.initializeFirebase();
     }
 
     public static void main(String[] args) {
